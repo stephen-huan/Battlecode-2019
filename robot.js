a75a6c9bd01d54f0a92f49fd5299d90bab0e853a
Luke Thistlethwaite <26513277+LThistle@users.noreply.github.com>
2019-01-08 18:35:17 -0500

    Basic movement towards goal
    Currently hardcoded to try to move towards (0,0), should also be noted that it does not navigate around concave obstacles

---
+++ robot.js
@@ -0,0 +1,53 @@
+import {
+  BCAbstractRobot,
+  SPECS
+} from 'battlecode';
+
+var step = -1;
+class MyRobot extends BCAbstractRobot {
+  turn() {
+    step++;
+    var map = this.map
+    if (this.me.unit === SPECS.CRUSADER) {
+      // this.log("Crusader health: " + this.me.health);
+      if (this.me.x == 0 && this.me.y == 0)
+        return
+      this.log("CRUSADER");
+      const choices = [
+        [-2, -2],[-2, -1],[-2, 0],[-2, 1],[-2, 2],[-1, -2],[-1, -1],[-1, 0],[-1, 1],[-1, 2],[0, -2],[0, -1],[0, 1],[0, 2],[1, -2],[1, -1],[1, 0],[1, 1],[1, 2],[2, -2],[2, -1],[2, 0],[2, 1],[2, 2]
+      ]
+      //Note: just a basic test, only navigates to top left corner atm
+      const goal_x = 0
+      const goal_y = 0
+      var min_dist = 999999
+      var best_move = [0, 0]
+      var count = 0
+      for (var i = 0; i < choices.length; i++) {
+        var x1 = this.me.x + choices[i][0]
+        var y1 = this.me.y + choices[i][1]
+        if (x1<0 || y1<0 || y1>=map.length || x1>=map.length)
+          continue
+        if (!map[y1][x1])
+          continue
+        var dist = Math.pow(Math.pow(goal_x - x1, 2) + Math.pow(goal_y - y1, 2), 0.5)
+        if (dist < min_dist) {
+            min_dist = dist
+            best_move = choices[i]
+            count += 1
+        }
+      }
+      if (count == 0)
+        return
+      return this.move(...best_move);
+    } else if (this.me.unit === SPECS.CASTLE) {
+      this.log("CASTLE");
+      if (step % 10 === 0)
+        return this.buildUnit(SPECS.CRUSADER, 1, 1);
+      else
+        return // this.log("Castle health: " + this.me.health);
+    }
+  }
+
+}
+
+var robot = new MyRobot();
