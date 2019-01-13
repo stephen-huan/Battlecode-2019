b09049ac35fe1237fd8a07f51520895cc1a9b5d9
Luke Thistlethwaite <lukethistle.va@gmail.com>
2019-01-12 19:47:45 -0500

    robots now harvest and return to castle with resources

--- main_program/robot.js
+++ main_program/robot.js
@@ -4,6 +4,14 @@ import {
 } from 'battlecode';
 //some general global vars
 var step = -1;
+var home_x
+var home_y
+var r1choices = [
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 0],
+]
 var r2choices = [
   [-1, -1],
   [-1, 0],
@@ -65,7 +73,14 @@ var castlelist = [] //castelist is a list of all castles on same team (self incl
 //crusader variables
 var goal_x //current x destination
 var goal_y //curent y destination
-var known_robots //list of all robots that have been seen by the crusader
+var known_robots = [] //list of all robots that have been seen by the crusader
+//PILGRIM
+var head_home=false
+var fullK = false
+var fullF = false
+var home_ID
+var dX
+var dY
 //hardcoded values for castle_talk transmission
 var removeFuel = 0b111 //value for a castle to remove a deposit from it's list of unvisited deposits
 var removeKarbonite = 0b101 //same as removeFuel but for Karbonite
@@ -84,6 +99,7 @@ class MyRobot extends BCAbstractRobot {
       return this.choose_move(goal_x, goal_y, choices)
 
     } else if (this.me.unit === SPECS.CASTLE) {
+      this.log("Start CASTLE TURN")
       if (step == 0) {
         var robotlist = this.getVisibleRobots()
         for (var i = 0; i < robotlist.length; i++)
@@ -130,52 +146,111 @@ class MyRobot extends BCAbstractRobot {
       else if (this.getRobot(castlelist[i][0]).castle_talk == removeKarbonite)
         klocations.splice(klocations.length - 1)
 
-      if (step > 2 && this.fuel >= 50 && this.karbonite >= 20) {
-        //this.log("length is "+r2choices.length)
-        var location = this.choose_spawn(this, r2choices)
+      //check if their are available fuel spots
+      var hasFuel = flocations.length>0
+      var hasKarbonite = klocations.length>0
+      if (step > 2 && this.fuel >= 50 && this.karbonite >= 20 && (hasFuel || hasKarbonite)) {
+        var location = this.choose_spawn(this, r1choices)
         if (location != undefined) {
           this.log('location is ' + location)
           var x = location[0]
           var y = location[1]
-          this.log('Building crusader at '+(x+this.me.x)+','+(y+this.me.y))
-          return this.buildUnit(SPECS.CRUSADER, x, y)
-          return
-        }
-        else
+          this.log('Building pilgrim at ' + (x + this.me.x) + ',' + (y + this.me.y))
+          if(hasKarbonite)
+            var goal_coords = klocations.splice(klocations.length - 1)
+          else
+            var goal_coords = flocations.splice(klocations.length - 1)
+          this.log('goal coords is '+goal_coords)
+          var val = goal_coords[0][0] * 100 + goal_coords[0][1]
+          this.signal(val,2)
+          return this.buildUnit(SPECS.PILGRIM, x, y)
+        } else
           this.log("no spawn locations available")
       }
 
     } else if (this.me.unit === SPECS.PILGRIM) {
-
+      this.log("Start PILGRIM TURN")
+      if(step==0)
+      {
+        this.set_home(this.me.x,this.me.y)
+        this.set_goal()
+      }
+      if(this.me.x==goal_x && this.me.y==goal_y) {
+        if(fullK || fullF)
+        {
+          this.log("depositing resource to ["+(this.me.x+dX)+','+(this.me.y+dY)+']')
+          this.log(this.me.karbonite+','+this.me.fuel)
+          fullK=false
+          fullF=false
+          //make em get out of the way when they've deposited resources
+          goal_x=0
+          goal_y=0
+          return this.give(dX,dY,this.me.karbonite,this.me.fuel)
+        }
+        if(this.me.fuel >= 100)
+        {
+          this.log("Full of fuel, heading home")
+          goal_x=home_x
+          goal_y=home_y
+          fullF=true
+          head_home=true
+        }
+        else if(this.me.karbonite >= 20) {
+          this.log("Full of karbonite, heading home")
+          goal_x=home_x
+          goal_y=home_y
+          fullK=true
+          head_home=true
+        }
+        else {
+          this.log("Harvesting resource.")
+          return this.mine()
+        }
+      }
+      return this.choose_move(goal_x,goal_y,r4choices)
     }
+  }
+  set_goal() {
+    var dest = this.coords_from_transmit(this)
+    goal_x=dest[0]
+    goal_y=dest[1]
+  }
+  set_home(x1, y1) {
+    home_x=x1
+    home_y=y1
   }
   get_dist(x1, y1, x2, y2) {
     return Math.pow(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2), 0.5)
   }
   coords_from_transmit(robot) {
-    //TODO: Fix to search for castle
-    var castle_robot = this.getVisibleRobotMap()[this.me.y][this.me.x - 1]
-    var transmission = this.getRobot(castle_robot).signal
-    goal_y = transmission % 100
-    goal_x = Math.floor((transmission - goal_y) / 100)
+    var visMap = robot.getVisibleRobots()
+    for (var i = 0; i < visMap.length; i++) {
+      var newUnit = visMap[i]
+      if (newUnit.unit == 0) {
+        this.log('Castle Found')
+        home_ID=newUnit.id
+        dX=newUnit.x-this.me.x
+        dY=newUnit.y-this.me.y
+        var transmission = newUnit.signal
+        this.log('transmission is ' + transmission)
+        var new_y = transmission % 100
+        var new_x = Math.floor((transmission - new_y) / 100)
+        return [new_x,new_y]
+      }
+    }
   }
 
   choose_spawn(robot, choices) {
-    //this.log("now length is "+choices.length)
     var obstacles = this.map
     var x1 = robot.me.x
     var y1 = robot.me.y
-    //this.log('pass 1')
     for (var val = 0; val < choices.length; val++) {
-      //this.log('pass 2')
-      //this.log(choices[val])
       var xPos = choices[val][0]
       var yPos = choices[val][1]
-      //this.log(xPos + ' ' + yPos)
       if (this.map[y1 + yPos][x1 + xPos] && this.getVisibleRobotMap()[yPos + y1][xPos + x1] <= 0)
         return choices[val]
       else
-        this.log("Spawn position "+(x1+xPos)+','+(y1+yPos)+' is occupied')
+        this.log("Spawn position " + (x1 + xPos) + ',' + (y1 + yPos) + ' is occupied')
     }
   }
   choose_move(goal_x, goal_y, choices) {
